6bfd30b3e5843e605a23761f69ec4b3674044587
Vu Long <vulong264@gmail.com>
2016-10-17 16:17:31 +0700

    implemented signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+	def create
+		#WRONG: @user = User.create(email: user_params[:email],password_digest: user_params[:password])
+		@user = User.new user_params
+		if @user.save
+			flash[:success] = "User created successfully"
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
+	end
+
+end
